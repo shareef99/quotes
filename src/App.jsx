fec847d1c0c4fb9b9ee190b29abdd1947627f150
shareef <nadeemshareef934@gmail.com>
2021-01-14 14:24:17 +0530

    fix production color error

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,35 @@ function App() {
     const [author, setAuthor] = useState(null);
     const [tags, setTags] = useState([]);
     const colors = [
-        "blueGray",
-        "coolGray",
-        "trueGray",
-        "gray",
-        "warmGray",
-        "red",
-        "orange",
-        "amber",
-        "yellow",
-        "lime",
-        "green",
-        "emerald",
-        "teal",
-        "cyan",
-        "lightBlue",
-        "blue",
-        "indigo",
-        "violet",
-        "purple",
-        "fuchsia",
-        "pink",
-        "rose",
+        { id: 1, bgColor: "bg-blueGray-300", textColor: "text-blueGray-500" },
+        { id: 2, bgColor: "bg-coolGray-300", textColor: "text-coolGray-500" },
+        { id: 3, bgColor: "bg-trueGray-300", textColor: "text-trueGray-500" },
+        { id: 4, bgColor: "bg-gray-300", textColor: "text-gray-500" },
+        { id: 5, bgColor: "bg-warmGray-300", textColor: "text-warmGray-500" },
+        { id: 6, bgColor: "bg-red-300", textColor: "text-red-500" },
+        { id: 7, bgColor: "bg-orange-300", textColor: "text-orange-500" },
+        { id: 8, bgColor: "bg-amber-300", textColor: "text-amber-500" },
+        { id: 9, bgColor: "bg-yellow-300", textColor: "text-yellow-500" },
+        { id: 10, bgColor: "bg-lime-300", textColor: "text-lime-500" },
+        { id: 11, bgColor: "bg-green-300", textColor: "text-green-500" },
+        { id: 12, bgColor: "bg-emerald-300", textColor: "text-emerald-500" },
+        { id: 13, bgColor: "bg-teal-300", textColor: "text-teal-500" },
+        { id: 14, bgColor: "bg-cyan-300", textColor: "text-cyan-500" },
+        {
+            id: 15,
+            bgColor: "bg-lightBlue-500",
+            textColor: "text-lightBlue-500",
+        },
+        { id: 16, bgColor: "bg-blue-300", textColor: "text-blue-500" },
+        { id: 17, bgColor: "bg-indigo-300", textColor: "text-indigo-500" },
+        { id: 18, bgColor: "bg-violet-300", textColor: "text-violet-500" },
+        { id: 19, bgColor: "bg-purple-300", textColor: "text-purple-500" },
+        { id: 20, bgColor: "bg-fuchsia-300", textColor: "text-fuchsia-500" },
+        { id: 21, bgColor: "bg-pink-300", textColor: "text-pink-500" },
+        { id: 22, bgColor: "bg-rose-300", textColor: "text-rose-500" },
     ];
-    const [color, setColor] = useState(colors[0]);
+    const [bgColor, setBgColor] = useState(colors[0]);
+    const [textColor, setTextColor] = useState(colors[0]);
 
     const getRandomQuoteAsync = async () => {
         const response = await axios.get("https://api.quotable.io/random");
@@ -47,25 +52,17 @@ function App() {
     const handleNextQuote = async () => {
         await getRandomQuoteAsync();
         let randomNumber = Math.floor(Math.random() * colors.length);
-        setColor(colors[randomNumber]);
+        setBgColor(colors[randomNumber].bgColor);
+        setTextColor(colors[randomNumber].textColor);
     };
     return (
         <>
             <section
-                className={`flex flex-col items-center justify-center h-screen transition-colors duration-500 delay-75 ease-in bg-${color}-300`}
+                className={`flex flex-col items-center justify-center h-screen transition-colors duration-500 delay-75 ease-in ${bgColor}`}
             >
                 <div
                     id="quote-box"
-                    className={`
-                    bg-white
-                    text-${color}-500 
-                    px-8 pt-8 
-                    min-w-9/2 max-w-9/10 
-                    sm:min-w-3/4 sm:max-w-3/4 
-                    md:min-w-6/10 md:max-w-6/10 
-                    lg:min-w-1/2 lg:max-w-1/2
-                    rounded-2xl
-                    `}
+                    className={`bg-white ${textColor} px-8 pt-8 min-w-9/2 max-w-9/10 sm:min-w-3/4 sm:max-w-3/4 md:min-w-6/10 md:max-w-6/10 lg:min-w-1/2 lg:max-w-1/2 rounded-2xl`}
                 >
                     {/*  */}
                     <div className="">
@@ -95,7 +92,7 @@ function App() {
                             id="tweet-quote"
                             rel="noreferrer"
                             title="Tweet amazing Quote!"
-                            className={`focus:outline-none bg-${color}-100 py-1 px-2 border-${color}-200 rounded-md`}
+                            className={`focus:outline-none ${bgColor} py-1 px-2 rounded-md`}
                         >
                             Tweet
                         </a>
@@ -115,13 +112,13 @@ function App() {
                             id="new-quote"
                             onClick={handleNextQuote}
                             title="next quote"
-                            className={`focus:outline-none bg-${color}-100 py-1 px-2 border-${color}-200 rounded-md`}
+                            className={`focus:outline-none ${bgColor} py-1 px-2 rounded-md`}
                         >
                             New quote
                         </button>
                     </div>
                 </div>
-                <div className={`pt-6 text-${color}-800 text-lg sm:text-xl`}>
+                <div className={`pt-6 ${textColor} text-lg sm:text-xl`}>
                     <p>
                         <a
                             href="https://portfolio.shareef.vercel.app"
